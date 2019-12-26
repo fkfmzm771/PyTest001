8eb49c26ad70ac99052a7f2ef8ece2079471e33f
JAPAN KLEEN-TEX <ktjpwuser@kleen-tex.jp>
2019-12-26 09:51:03 +0900

    Initial commit

---
+++ venv/Include/first_fy.py
@@ -0,0 +1,28 @@
+print("printTest01")
+print("printTest02")
+print("printTest03")
+print("printTest04")
+print("printTest05")
+print("printTest06")
+
+print('d','d''f','f','o',sep='|')
+
+print(2333,444,555,666,sep='-')
+
+print("{:_>10}".format('asdf'))
+
+
+a = 8000
+b = 8000 * 2
+b = 8000
+
+
+print(id(a))
+print(id(b))
+
+# Camel case  첫글자 소문자  메소드
+# Pacal case   첫글자 대문자  클래스
+# Snake case 언더바로 연결   변수 선언에 주로 사용
+
+
+
